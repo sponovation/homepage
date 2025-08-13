04efe90a50f9d2d48605e1ec4459e83be29d6203
Younghoon Kim <eth220421@gmail.com>
2025-08-13 19:51:56 +0900

    feat: 영어 번역 페이지 추가
    ✅ 작업 내용
    - /html/ko, /html/en 폴더 분리하여 영어 페이지 생성
    - index.html 접근 시, 브라우저 언어 감지하여 적절한 언어 페이지로 라우팅

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,142 +4,133 @@
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
 
-(function($) {
+(function ($) {
+  var $window = $(window),
+    $body = $("body"),
+    $header = $("#header"),
+    $banner = $("#banner");
 
-	var	$window = $(window),
-		$body = $('body'),
-		$header = $('#header'),
-		$banner = $('#banner');
+  // 브라우저 언어 감지 후 일치하는 언어 index.html로 이동
+  var userLang = navigator.language || navigator.userLanguage;
+  console.log("브라우저 언어:", userLang);
 
-	// Breakpoints.
-		breakpoints({
-			xlarge:	'(max-width: 1680px)',
-			large:	'(max-width: 1280px)',
-			medium:	'(max-width: 980px)',
-			small:	'(max-width: 736px)',
-			xsmall:	'(max-width: 480px)'
-		});
+  // ko-KR이 아닌 경우 영문 페이지로 이동
+  if (userLang.toLowerCase() !== "ko-kr") {
+    if (!window.location.pathname.includes("html/en")) {
+      window.location.href = "/html/en/index.html";
+    }
+  }
 
-	// Play initial animations on page load.
-		$window.on('load', function() {
-			window.setTimeout(function() {
-				$body.removeClass('is-preload');
-			}, 100);
-		});
+  // Breakpoints.
+  breakpoints({
+    xlarge: "(max-width: 1680px)",
+    large: "(max-width: 1280px)",
+    medium: "(max-width: 980px)",
+    small: "(max-width: 736px)",
+    xsmall: "(max-width: 480px)",
+  });
 
-	// Header.
-		if ($banner.length > 0
-		&&	$header.hasClass('alt')) {
+  // Play initial animations on page load.
+  $window.on("load", function () {
+    window.setTimeout(function () {
+      $body.removeClass("is-preload");
+    }, 100);
+  });
 
-			$window.on('resize', function() { $window.trigger('scroll'); });
+  // Header.
+  if ($banner.length > 0 && $header.hasClass("alt")) {
+    $window.on("resize", function () {
+      $window.trigger("scroll");
+    });
 
-			$banner.scrollex({
-				bottom:		$header.outerHeight(),
-				terminate:	function() { $header.removeClass('alt'); },
-				enter:		function() { $header.addClass('alt'); },
-				leave:		function() { $header.removeClass('alt'); }
-			});
+    $banner.scrollex({
+      bottom: $header.outerHeight(),
+      terminate: function () {
+        $header.removeClass("alt");
+      },
+      enter: function () {
+        $header.addClass("alt");
+      },
+      leave: function () {
+        $header.removeClass("alt");
+      },
+    });
+  }
 
-		}
+  // Menu.
+  var $menu = $("#menu");
 
-	// Menu.
-		var $menu = $('#menu');
+  $menu._locked = false;
 
-		$menu._locked = false;
+  $menu._lock = function () {
+    if ($menu._locked) return false;
 
-		$menu._lock = function() {
+    $menu._locked = true;
 
-			if ($menu._locked)
-				return false;
+    window.setTimeout(function () {
+      $menu._locked = false;
+    }, 350);
 
-			$menu._locked = true;
+    return true;
+  };
 
-			window.setTimeout(function() {
-				$menu._locked = false;
-			}, 350);
+  $menu._show = function () {
+    if ($menu._lock()) $body.addClass("is-menu-visible");
+  };
 
-			return true;
+  $menu._hide = function () {
+    if ($menu._lock()) $body.removeClass("is-menu-visible");
+  };
 
-		};
+  $menu._toggle = function () {
+    if ($menu._lock()) $body.toggleClass("is-menu-visible");
+  };
 
-		$menu._show = function() {
+  $menu
+    .appendTo($body)
+    .on("click", function (event) {
+      event.stopPropagation();
 
-			if ($menu._lock())
-				$body.addClass('is-menu-visible');
+      // Hide.
+      $menu._hide();
+    })
+    .find(".inner")
+    .on("click", ".close", function (event) {
+      event.preventDefault();
+      event.stopPropagation();
+      event.stopImmediatePropagation();
 
-		};
+      // Hide.
+      $menu._hide();
+    })
+    .on("click", function (event) {
+      event.stopPropagation();
+    })
+    .on("click", "a", function (event) {
+      var href = $(this).attr("href");
 
-		$menu._hide = function() {
+      event.preventDefault();
+      event.stopPropagation();
 
-			if ($menu._lock())
-				$body.removeClass('is-menu-visible');
+      // Hide.
+      $menu._hide();
 
-		};
+      // Redirect.
+      window.setTimeout(function () {
+        window.location.href = href;
+      }, 350);
+    });
 
-		$menu._toggle = function() {
+  $body
+    .on("click", 'a[href="#menu"]', function (event) {
+      event.stopPropagation();
+      event.preventDefault();
 
-			if ($menu._lock())
-				$body.toggleClass('is-menu-visible');
-
-		};
-
-		$menu
-			.appendTo($body)
-			.on('click', function(event) {
-
-				event.stopPropagation();
-
-				// Hide.
-					$menu._hide();
-
-			})
-			.find('.inner')
-				.on('click', '.close', function(event) {
-
-					event.preventDefault();
-					event.stopPropagation();
-					event.stopImmediatePropagation();
-
-					// Hide.
-						$menu._hide();
-
-				})
-				.on('click', function(event) {
-					event.stopPropagation();
-				})
-				.on('click', 'a', function(event) {
-
-					var href = $(this).attr('href');
-
-					event.preventDefault();
-					event.stopPropagation();
-
-					// Hide.
-						$menu._hide();
-
-					// Redirect.
-						window.setTimeout(function() {
-							window.location.href = href;
-						}, 350);
-
-				});
-
-		$body
-			.on('click', 'a[href="#menu"]', function(event) {
-
-				event.stopPropagation();
-				event.preventDefault();
-
-				// Toggle.
-					$menu._toggle();
-
-			})
-			.on('keydown', function(event) {
-
-				// Hide on escape.
-					if (event.keyCode == 27)
-						$menu._hide();
-
-			});
-
+      // Toggle.
+      $menu._toggle();
+    })
+    .on("keydown", function (event) {
+      // Hide on escape.
+      if (event.keyCode == 27) $menu._hide();
+    });
 })(jQuery);
